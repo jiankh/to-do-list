8e74ff72b312c1cccc24e7b7a06ae270043276c8
jiankh <131821436+jiankh@users.noreply.github.com>
2023-09-23 15:38:17 -0400

    base webpack files added

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', 
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/menu/',
+          },
+        },
+      },
+    ],
+  },
+};
