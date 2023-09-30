91741dc9c3a69ec19aa31a0b8b24f26def552e87
jiankh <131821436+jiankh@users.noreply.github.com>
2023-09-30 13:06:07 -0400

    fixed event listner for adding project

--- src/index.js
+++ src/index.js
@@ -22,58 +22,58 @@ let toDoArray = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
 
 
 
-// EventListenerModule.init();
+EventListenerModule.init();
 
 
-// //project section
+//project section
 
-// const projectContainer = document.querySelector("[data-projects-list]");
-
-
-// function saveProject() {
-//     localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projectArray))
-// }
-
-// function saveAndRenderProject() {
-//     saveProject()
-//     renderProject()
-// }
-
-// function renderProject() {
-//     clearElement(projectContainer);
-//     projectArray.forEach((project) => {
-//         const projectElement = document.createElement("li");
-//         projectElement.dataset.projectId = project.id;
-//         projectElement.classList.add("project-name");
-//         projectElement.innerText = project.name;
-//         projectContainer.appendChild(projectElement)
-//     })
-// }
-
-// function clearElement(element) {
-//     element.innerHTML = "";
-// }
+const projectContainer = document.querySelector("[data-projects-list]");
 
 
-// //NEW PROJECT BUTTON ADD
-// const newProjectForm = document.querySelector("[data-new-project-form]")
-// const newProjectInput = document.querySelector("[data-new-project-input]")
+function saveProject() {
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projectArray))
+}
 
-// newProjectForm.addEventListener("submit", (e) => {
-//     e.preventDefault()
-//     const projectName = newProjectInput.value;
-//     if (projectName == null || projectName === "") return //if empty name dont make the project
-//     const project = createProject(projectName);
-//     newProjectInput.value = null; //clear the form
-//     projectArray.push(project);
-//     saveAndRenderProject()
-// })
+function saveAndRenderProject() {
+    saveProject()
+    renderProject()
+}
 
-// function createProject(name) {
-//     return new Project(name)
-// }
+function renderProject() {
+    clearElement(projectContainer);
+    projectArray.forEach((project) => {
+        const projectElement = document.createElement("li");
+        projectElement.dataset.projectId = project.id;
+        projectElement.classList.add("project-name");
+        projectElement.innerText = project.name;
+        projectContainer.appendChild(projectElement)
+    })
+}
 
-// renderProject()
+function clearElement(element) {
+    element.innerHTML = "";
+}
+
+
+//NEW PROJECT BUTTON ADD
+const newProjectForm = document.querySelector("[data-new-project-form]")
+const newProjectInput = document.querySelector("[data-new-project-input]")
+
+newProjectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const projectName = newProjectInput.value;
+    if (projectName == null || projectName === "") return //if empty name dont make the project
+    const project = createProject(projectName);
+    newProjectInput.value = null; //clear the form
+    projectArray.push(project);
+    saveAndRenderProject()
+})
+
+function createProject(name) {
+    return new Project(name)
+}
+
+renderProject()
 
 
 // //TASK RENDER
